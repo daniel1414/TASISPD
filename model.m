c72ef148075f207600c26363d6d3b4dcb8dc5b92
Daniel <dcichon369@gmail.com>
2020-05-23 20:35:38 +0200

    podzial zadan

---
+++ model.m
@@ -0,0 +1,19 @@
+% model robota przeszukuj¹cego pod³o¿e w celu znalezienia z³ó¿ ropy
+% naftowej.
+
+bateria = 100;
+WODA = 0;
+ZIEMIA = 1;
+POWIETRZE = 2;
+
+SRODOWISKO = ZIEMIA;
+
+switch SRODOWISKO
+    case WODA
+        % Mariusz
+    case ZIEMIA
+        % Daniel
+    case POWIETRZE
+        
+    otherwise
+end
